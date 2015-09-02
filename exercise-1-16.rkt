061db5d1d36f9b6cc4763bdcc1fad065d825686a
stefan <stefan@twitter.com>
2015-09-02 13:42:09 +0100

    Use quotient for integer division [1.16]

--- exercise-1-16.rkt
+++ exercise-1-16.rkt
@@ -9,9 +9,10 @@
 (define (fast-expt b n)
   (define (iter ibit pb result)
     (define grow (square pb))
+    (define travel-i (quotient ibit 2))
     (cond ((= ibit 0) result)
-          ((even? ibit) (iter (/ ibit 2) grow result))
-          (else (iter (/ (dec ibit) 2) grow (* pb result)))))
+          ((even? ibit) (iter travel-i grow result))
+          (else (iter travel-i grow (* pb result)))))
   (iter n b 1))
 
 (fast-expt 2 4)
